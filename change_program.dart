0ce0cb412204f693ebb3e340b2e3913cff9cb4d8
bigboss27051 <tanawut.bigboss@gmail.com>
2020-10-06 17:17:11 +0700

    fix bug calulate change money

--- change_program.dart
+++ change_program.dart
@@ -1,7 +1,16 @@
+import 'dart:io';
+
 void main() {
-  int total = 125;
-  int moneyCustomer = 500;
-  int leftChange = moneyCustomer - total;
+  print("Enter total price");
+  String totalInput = stdin.readLineSync();
+
+  print("Enter customer money");
+  String customerMoneyInput = stdin.readLineSync();
+
+  //parse string to int
+  int total = int.parse(totalInput);
+  int customerMoney = int.parse(customerMoneyInput);
+  int leftChange = customerMoney - total;
 
   List<int> arrMoneyList = [1, 2, 5, 10, 20, 50, 100, 500];
   int sizeOfArr = arrMoneyList.length;
@@ -10,7 +19,7 @@ void main() {
 
 void changeMoney(List<int> arrMoneyList, int leftChange, int sizeOfArr) {
   if (leftChange >= arrMoneyList[sizeOfArr - 1]) {
-    final result = (leftChange / arrMoneyList[sizeOfArr - 1]).round();
+    final result = (leftChange / arrMoneyList[sizeOfArr - 1]).floor();
     leftChange = leftChange % arrMoneyList[sizeOfArr - 1];
     if (result > 0) {
       print('Change ${arrMoneyList[sizeOfArr - 1]}: ${result}');
